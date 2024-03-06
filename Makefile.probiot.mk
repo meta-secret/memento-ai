4dbd66a6ff26cdd440885257d8c634fc04c5b7ff
metabynull <bynull@meta-secret.org>
2024-03-06 01:26:06 -0800

    Generify make files

--- Makefile.probiot.mk
+++ Makefile.probiot.mk
@@ -1,28 +1,31 @@
+APP_NAME := probiot_t1000
+CORE_DIR := nervo_core
+APP_DIR := probiot
 
 app_clean:
-	cd nervo_core && rm -rf target
-	cd nervo_core && rm -rf Cargo.lock
+	cd ${CORE_DIR} && rm -rf target
+	cd ${CORE_DIR} && rm -rf Cargo.lock
 
-	cd probiot && rm -rf target
-	cd probiot && rm -rf Cargo.lock
+	cd ${APP_DIR} && rm -rf target
+	cd ${APP_DIR} && rm -rf Cargo.lock
 
 app_build:
-	cd probiot && cargo build --release
+	cd ${APP_DIR} && cargo build --release
 
 docker_build: app_clean
 	#docker build -t nervo_bot:latest --progress=plain .  2>&1 | tee docker-build.log
-	docker build -f Probiot.dockerfile -t probiot_t1000:latest .
+	docker build -f Probiot.dockerfile -t ${APP_NAME}:latest .
 
 docker_run: docker_build
-	docker run -ti --name probiot_t1000 probiot_t1000:latest
+	docker run -ti --name ${APP_NAME} ${APP_NAME}:latest
 
 docker_run_daemon: docker_build
-	docker run -d --name probiot_t1000 probiot_t1000:latest
+	docker run -d --name ${APP_NAME} ${APP_NAME}:latest
 
 docker_kill:
-	docker kill probiot_t1000 || true
+	docker kill ${APP_NAME} || true
 
 docker_rm:
-	docker container rm probiot_t1000 || true
+	docker container rm ${APP_NAME} || true
 
 docker_clean_up: docker_kill docker_rm
--- Makefile.r2d2.mk
+++ Makefile.r2d2.mk
@@ -1,25 +1,31 @@
+APP_NAME := nervo_bot_r2d2
+CORE_DIR := nervo_core
+APP_DIR := nervo_bot_app
 
 app_clean:
-	cd nervo_bot_app && rm -rf target
-	cd nervo_bot_app && rm -rf Cargo.lock
-	cd nervo_core && rm -rf target
-	cd nervo_core && rm -rf Cargo.lock
+	cd ${CORE_DIR} && rm -rf target
+	cd ${CORE_DIR} && rm -rf Cargo.lock
+
+	cd ${APP_DIR} && rm -rf target
+	cd ${APP_DIR} && rm -rf Cargo.lock
 
 app_build:
-	cd nervo_bot_app && cargo build --release
+	cd ${APP_DIR} && cargo build --release
 
 docker_build: app_clean
-	#docker build -t nervo_bot:latest --progress=plain .  2>&1 | tee docker-build.log
-	docker build -t nervo_bot:latest .
+	#docker build -t ${APP_NAME}:latest --progress=plain .  2>&1 | tee docker-build.log
+	docker build -f Probiot.dockerfile -t ${APP_NAME}:latest .
 
 docker_run: docker_build
-	docker run -ti --name nervo_bot_dk nervo_bot:latest
+	docker run -ti --name ${APP_NAME} ${APP_NAME}:latest
 
 docker_run_daemon: docker_build
-	docker run -d --name nervo_bot_dk nervo_bot:latest
+	docker run -d --name ${APP_NAME} ${APP_NAME}:latest
 
-docker_stop:
-	docker kill nervo_bot_dk || true
+docker_kill:
+	docker kill ${APP_NAME} || true
 
-docker_clean:
-	docker container rm nervo_bot_dk || true
+docker_rm:
+	docker container rm ${APP_NAME} || true
+
+docker_clean_up: docker_kill docker_rm
