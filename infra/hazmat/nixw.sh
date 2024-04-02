c8b5e0dbd9ae2f33f5e79cf74c48f39a49307310
cypherkitty <bynull@meta-secret.org>
2024-04-01 22:45:25 -0700

    gh actions, deployment: improve deployment.yml

--- infra/hazmat/nixw.sh
+++ infra/hazmat/nixw.sh
@@ -7,13 +7,5 @@ if ! command -v nix-env &> /dev/null; then
     echo "Nix is not installed. Installing..."
     # Install Nix
     sh <(curl -L https://nixos.org/nix/install) --no-daemon
-fi
-
-# Enter the project-specific environment
-if [ -e "shell.nix" ]; then
-    echo "Entering project-specific environment..."
-    nix-shell
-else
-    echo "No shell.nix found in the current directory."
-    echo "Please create a shell.nix file to specify the project environment."
+    . "${HOME}"/.nix-profile/etc/profile.d/nix.sh
 fi
