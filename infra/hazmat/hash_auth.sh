1665ad360ca2c57a77216f8549cedbefcac925d9
cypherkitty <bynull@meta-secret.org>
2024-04-10 23:27:16 -0700

    infra, devbox: provide "auth" task to log in into vault easily

--- infra/hazmat/hash_auth.sh
+++ infra/hazmat/hash_auth.sh
@@ -2,16 +2,13 @@
 
 set -euo pipefail
 
+mkdir -p tmp
+
 # Prompt the user to enter a password without echoing it to the terminal
 echo -n "Enter your password: "
 read -sr password
 echo
 
 # Calculate the SHA-256 hash of the password
-echo -n "$password" | sha256sum | awk '{print $1}'
-
-echo "Screen will be cleared in (here) 30 seconds."
-
-for _ in {1..30}; do read -rs -n1 -t1 || printf ".";done;echo
-
-clear
+HASH=$(echo -n "$password" | sha256sum | awk '{print $1}')
+echo NERVOSET_VOID_SENTINEL=\""${HASH}"\"> tmp/.env
